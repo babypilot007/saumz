09c4a8106d4e5b2fdc9ac256ca6b4f8398477702
babypilot007 <skypirateee@gmail.com>
2024-11-12 14:45:40 -0500

    intro

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Babi Saumz</h1>
+        <h2>Welcome to my Cult!</h2>
+        <p>All Donations are accepted.</p>
         
       </header>
     </div>
